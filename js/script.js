bb76451c0ed1310f6c705e152819482f5edd271e
A-F-2 <dualac26@gmail.com>
2024-07-19 02:15:23 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+﻿let menuElem = document.querySelector(".context-menu")
+
+document.body.addEventListener("contextmenu" , function (event) {
+    event.preventDefault()
+    menuElem.style.display = "block"
+    menuElem.style.left = event.pageX + "px"
+    menuElem.style.top = event.pageY + "px"
+})
+
+document.body.addEventListener("click" , function () {
+    menuElem.style.display = "none"
+    
+})
+document.body.addEventListener("keydown" , function (event) {
+    if (event.key === "Escape") {
+        menuElem.style.display = "none"
+    }
+})
